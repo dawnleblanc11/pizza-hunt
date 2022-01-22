a57af04f7acca022516aa1dda5b31544ca29c3c0
Dawn LeBlanc <dawnleblanc11@gmail.com>
2022-01-22 13:00:35 -0500

    CRUD pizza data

---
+++ models/Pizza.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+
+const PizzaSchema = new Schema({
+    pizzaName: {
+      type: String
+    },
+    createdBy: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    size: {
+      type: String,
+      default: 'Large'
+    },
+    toppings: [],
+    comments: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Comment'
+        }
+      ]
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false
+  });
+
+  // create the Pizza model using the PizzaSchema
+const Pizza = model('Pizza', PizzaSchema);
+
+// export the Pizza model
+module.exports = Pizza;
